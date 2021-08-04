720e33861cb8951313cf4175b6183ad2b82f9777
Jerry <jyji@telenavsoftware.com>
2021-08-04 11:20:39 +0800

    add new feature

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@ vod featureDev()
     std::cout << "dev.feature.jjy" << std::endl;
 }
 
+void newFeature()
+{
+    std::cout << "dev.newfeature.jjy" << std::endl;
+}
 
 int main(int argc, char * )
 {
