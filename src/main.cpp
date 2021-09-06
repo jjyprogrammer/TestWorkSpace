d42fec76c1f3f9037e7981ca5e1c5bd9fc77b4ae
Jerry <jyji@telenavsoftware.com>
2021-09-06 10:56:00 +0800

    add cherry pick test

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@ vod featureDev()
 
 int main(int argc, char * )
 {
-    
+    std::cout << "cherry pick test" << std::endl;
+    std::cout << "cherry pick" << std::endl;
+    std::cout << "hello world" << std::endl;
     return 0;
 }
